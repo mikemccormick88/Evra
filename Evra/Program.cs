23d9a66cfa1b534275716502274aa8377f5e5fab
mikemccormick88 <mike.rory.mccormick@gmail.com>
2020-07-23 04:41:42 +0200

    Add project files.

---
+++ Evra/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+
+namespace EvraAutomatedTests
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Program Program = new Program();
+            Console.WriteLine("g for google, f for firefox, i for ie, esc to quit");
+            string go = Console.ReadLine();
+            if (go == "g")
+            {
+                IWebDriver driver = new ChromeDriver();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                Program.loadLoginPage(driver);
+                Program.sumbitLoginDetails(driver);
+                Program.submitSearch(driver);
+                Console.ReadLine();
+            }
+            else if (go == "f")
+            {
+                IWebDriver driver = new FirefoxDriver();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                Program.loadLoginPage(driver);
+                Program.sumbitLoginDetails(driver);
+                Program.submitSearch(driver);
+                Console.ReadLine();
+            }
+            else if (go == "i")
+            {
+                InternetExplorerOptions ieoptions = new InternetExplorerOptions();
+                ieoptions.IgnoreZoomLevel = true;
+                ieoptions.BrowserCommandLineArguments = "-private";
+                IWebDriver driver = new InternetExplorerDriver(ieoptions);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+                Program.loadLoginPage(driver);
+                Program.sumbitLoginDetails(driver);
+                Program.submitSearch(driver);
+                Console.ReadLine();
+            }
+        }
+        public void loadLoginPage(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl("https://evra.geophy.com/login");
+            driver.Manage().Window.Maximize();
+        }
+
+        public void sumbitLoginDetails(IWebDriver driver)
+        {
+            if (isLoginPageLoaded(driver))
+            {
+                //find and populate username
+                IWebElement userTextBox = driver.FindElement(By.XPath("//*[@id='email']"));
+                userTextBox.SendKeys("qaskillschallenge@geophy.com");
+
+                //find and populate password
+                IWebElement passwordTextBox = driver.FindElement(By.XPath("//*[@id='password']"));
+                passwordTextBox.SendKeys("qaskillschallenge@geophy.com");
+
+                //submit form
+                passwordTextBox.Submit();
+            }
+            else
+            {
+
+            }
+        }
+
+        public void submitSearch(IWebDriver driver)
+        {
+            if (isSearchPageLoaded(driver))
+            {
+                IWebElement addressInput = driver.FindElement(By.Id("address_input"));
+                addressInput.SendKeys("555 N. College Avenue, Tempe, AZ, 85281");
+
+                if (driver.FindElements(By.ClassName("pac-matched")).Count() != 0)
+                {
+                    addressInput.SendKeys(Keys.ArrowDown);
+                    addressInput.SendKeys(Keys.Return);
+                }
+
+                IWebElement noiInput = driver.FindElement(By.Id("noi"));
+                noiInput.SendKeys("2000000");
+
+                IWebElement numberOfUnitsInput = driver.FindElement(By.XPath("//*[@name='number_of_units']"));
+                numberOfUnitsInput.SendKeys("200");
+
+                IWebElement yearInput = driver.FindElement(By.XPath("//*[@name='year_built']"));
+                yearInput.SendKeys("2000");
+
+                IWebElement occupancyInput = driver.FindElement(By.XPath("//*[@name='occupancy']"));
+                occupancyInput.SendKeys("80");
+
+                IWebElement submitButton = driver.FindElement(By.Id("introjsRunValuationButton"));
+                submitButton.Click();
+            }
+            else
+            {
+
+            }
+        }
+
+        public Boolean isLoginPageLoaded(IWebDriver driver)
+        {
+            Boolean isLoaded = false;
+            if (driver.FindElements(By.XPath("//*[@id='email']")).Count() != 0)
+            {
+                isLoaded = true;
+            }
+            return isLoaded;
+        }
+
+        public Boolean isSearchPageLoaded(IWebDriver driver)
+        {
+            Boolean isLoaded=false;
+            if (driver.FindElements(By.Id("introjsAddressInputForm")).Count() != 0)
+                {
+                    isLoaded = true;
+                }
+            return isLoaded;
+        }
+
+        public Boolean isResultsPageLoaded(IWebDriver driver)
+        {
+            Boolean isLoaded = false;
+            if (driver.FindElements(By.Id("property-section")).Count() != 0)
+            {
+                isLoaded = true;
+            }
+            return isLoaded;
+        }
+    }
+}
---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EvraAutomatedTests;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void chromeLoginTest()
+        {
+            Boolean pass = false;
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Program program = new Program();
+            program.loadLoginPage(driver);
+            program.sumbitLoginDetails(driver);
+            if (program.isSearchPageLoaded(driver))
+            {
+                pass = true;
+            }
+            if (pass == false)
+            {
+                string filename = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+                image.SaveAsFile("C:\\Users\\Mike.McCormick\\Desktop\\Testing\\" + filename + ".png", ScreenshotImageFormat.Png);
+            }
+            driver.Close();
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void chromeSubmitValuationTest()
+        {
+            Boolean pass = false;
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Program program = new Program();
+            program.loadLoginPage(driver);
+            program.sumbitLoginDetails(driver);
+            program.submitSearch(driver);
+            if (program.isResultsPageLoaded(driver))
+            {
+                pass = true;
+            }
+            if (pass == false)
+            {
+                string filename = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+                image.SaveAsFile("C:\\Users\\Mike.McCormick\\Desktop\\Testing\\" + filename + ".png", ScreenshotImageFormat.Png);
+            }
+            driver.Close();
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void fireFoxLoginTest()
+        {
+            Boolean pass = false;
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Program program = new Program();
+            program.loadLoginPage(driver);
+            program.sumbitLoginDetails(driver);
+            if (program.isSearchPageLoaded(driver))
+            {
+                pass = true;
+            }
+            if (pass == false)
+            {
+                string filename = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+                image.SaveAsFile("C:\\Users\\Mike.McCormick\\Desktop\\Testing\\" + filename + ".png", ScreenshotImageFormat.Png);
+            }
+            driver.Close();
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void fireFoxSubmitValuationTest()
+        {
+            Boolean pass = false;
+            IWebDriver driver = new FirefoxDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Program program = new Program();
+            program.loadLoginPage(driver);
+            program.sumbitLoginDetails(driver);
+            program.submitSearch(driver);
+            if (program.isResultsPageLoaded(driver))
+            {
+                pass = true;
+            }
+            if (pass == false)
+            {
+                string filename = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+                image.SaveAsFile("C:\\Users\\Mike.McCormick\\Desktop\\Testing\\" + filename + ".png", ScreenshotImageFormat.Png);
+            }
+            driver.Close();
+            Assert.IsTrue(pass);
+        }
+
+        [TestMethod]
+        public void IeLoginTest()
+        {
+            Boolean pass = false;
+            InternetExplorerOptions ieoptions = new InternetExplorerOptions();
+            ieoptions.IgnoreZoomLevel = true;
+            ieoptions.EnsureCleanSession = true;
+            ieoptions.BrowserCommandLineArguments = "-private";
+            IWebDriver driver = new InternetExplorerDriver(ieoptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Manage().Cookies.DeleteAllCookies();
+            Program program = new Program();
+            program.loadLoginPage(driver);
+            program.sumbitLoginDetails(driver);
+            if (program.isSearchPageLoaded(driver))
+            {
+                pass = true;
+            }
+            if (pass == false)
+            {
+                string filename = System.Reflection.MethodBase.GetCurrentMethod().Name;
+                Screenshot image = ((ITakesScreenshot)driver).GetScreenshot();
+                image.SaveAsFile("C:\\Users\\Mike.McCormick\\Desktop\\Testing\\"+filename+".png", ScreenshotImageFormat.Png);
+            }
+            driver.Close();
+            Assert.IsTrue(pass);
+        }
+    }
+}
